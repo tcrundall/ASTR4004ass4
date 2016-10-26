5ff3db2b3b6692e6ff0abee492506b5c7de3cfa4
Tim Crundall <tim.crundall@gmail.com>
2016-10-27 02:54:07 +1100

    Q3.1 done

--- q3.py
+++ q3.py
@@ -14,8 +14,8 @@ def f(t, q, args):
   return dqdt
 
 
-def get_motion(k=1, m=1, x0=1, N=100, T=20):
-  q0 = np.array([1.0, 0.0])
+def get_motion(k=1, m=1, x0=1, T=20, N=100):
+  q0 = np.array([x0, 0.0])
   t0 = 0.0
   
   # Set up the integrator
@@ -25,7 +25,7 @@ def get_motion(k=1, m=1, x0=1, N=100, T=20):
   integrator.set_initial_value(q0, t0).set_f_params([k,m])
   
   # Integrate
-  dt = 0.05
+  dt = 0.01
   #dt = T*1.0/(N-1.0)
   nstep = int(T/dt)
   #nstep = N
@@ -36,11 +36,33 @@ def get_motion(k=1, m=1, x0=1, N=100, T=20):
   for i in range(nstep):
     qsol[i+1,:] = integrator.integrate(integrator.t+dt)
   
-  print("Integration went well")
-  # Plot
-  plt.plot(qsol[:,0], qsol[:,1])
-  #plt.axis('equal')
-  plt.show()
-  return qsol
+  result = np.zeros((N,3))
+  # getting the N position and velocity values at equally spaced
+  # time intervals.
+  for i in range(N):
+    result[i][0] = i*T/(N-1.0)
+    closest_ix = np.ceil(result[i][0]/dt)
+    result[i][1] = qsol[closest_ix][0]
+    result[i][2] = qsol[closest_ix][1]
 
-print(get_motion())
+  return result 
+
+def xsol(t, k=1.0, m=1.0, x0=1):
+  #return np.sin(np.sqrt(k/m)*(t + np.sqrt(m/k)*np.arcsin(x0)))
+  return x0 * np.cos(np.sqrt(k/m)*t)
+
+number_of_points = 100
+max_time = 5
+
+ts = np.linspace(0,max_time,10*number_of_points)
+
+#plotting results for k=50, x0=0.1, m=1.0
+plt.plot(ts, xsol(ts, 50.0, 1.0, 0.1), label="Analytical position")
+result = get_motion(50.0, 1.0, 0.1, max_time, number_of_points)
+plt.plot(result[:,0], result[:,1], label="Simulated position")
+plt.plot(result[:,0], result[:,2], label="Simulated velocity")
+plt.legend()
+plt.xlabel("Times (s)")
+plt.ylabel("Velocity (m/s) and postion (m)")
+plt.savefig("linearspring.png")
+plt.show()
