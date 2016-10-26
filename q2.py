18f3aa6ac35000a05681e7fb9c145656b8b477af
Tim Crundall <tim.crundall@gmail.com>
2016-10-27 00:48:21 +1100

    Tidied q2.py

--- q2.py
+++ q2.py
@@ -1,6 +1,4 @@
 #! /usr/bin/env python
-
-#import assignment_hints as ah
 import ah
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,11 +9,6 @@ a_false = [0.5,1,2,1,0]
 times, v_with_errors = ah.simulate_v()
 x=np.linspace(min(times), max(times), 100)
 
-
 plt.plot(times, v_with_errors, 'o')
 plt.plot(x, ah.v_func(a_true, x))
 plt.savefig("figure1.pdf")
-plt.show()
-
-print(ah.lnprob_v_func(a_true, times, v_with_errors))
-print(ah.lnprob_v_func(a_false, times, v_with_errors))
